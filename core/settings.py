fd05aa9aefa0b9620bb576bb855fc40f122269e9
Shymanovskyi Vladyslav <112189462+VlaDisLav23232@users.noreply.github.com>
2025-04-26 00:23:31 +0300

    fixing an issue with photos

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Application definition
 
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
